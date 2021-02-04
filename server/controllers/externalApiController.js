880cd7da57a39438085ae44a023703d341860ef2
Burhan Yusuf <burhanyusuf@outlook.com>
2021-02-05 00:06:13 +0700

    Add coord to Hospital Data Endpoint

--- server/controllers/externalApiController.js
+++ server/controllers/externalApiController.js
@@ -2,25 +2,36 @@ const axios = require('axios')
 // apikey = 8fcf50414b509cf1b2eb3256968048fb
 class externalApiController{
 
-    static getDataHospital(req,res,next){
-        const prov = 'Aceh'
-        axios({
-            method: 'GET',
-            url: 'https://dekontaminasi.com/api/id/covid19/hospitals'
-        }).then(dataHospital => {
+    static async getDataHospital(req,res,next){
+        try{
+            const prov = 'DKI Jakarta'
+            let dataHospital = await axios({
+                method: 'GET',
+                url: 'https://dekontaminasi.com/api/id/covid19/hospitals'
+            });
             let hospitals = dataHospital.data.filter(hospital => hospital.province == prov);
-            hospitals.forEach(hospital => {
-                axios({
+            for(let hospital of hospitals){
+                let queryString = hospital.name.replace(/ +(?= )/g,'') // remove double-space
+                if(!queryString.includes(hospital.province.toUpperCase())){
+                    queryString += ' ' + hospital.province
+                }
+                let geoCodeReq = await axios({
                     method : 'GET',
-                    url : 'http://api.positionstack.com/v1/forward?access_key=8fcf50414b509cf1b2eb3256968048fb&limit=1&country=ID&query='+hospital.name
-                }).then(data => {
-                    console.log(hospital.name,data.data);
-                })
-            })
+                    url : 'https://geocode.search.hereapi.com/v1/geocode?apiKey=2kizGqqAPl8BcyDYpsfRfVazaDvgEhfIUPKmD6_bmtw&q='+queryString
+                });
+                hospital.latitude = 0
+                hospital.longtitude = 0
+                if(geoCodeReq.data.items.length > 0){
+                    hospital.latitude = geoCodeReq.data.items[0].position.lat
+                    hospital.longtitude = geoCodeReq.data.items[0].position.lng
+                }
+            }
             res.status(200).json(hospitals);
-        }).catch((err) => {
-            next(err)
-        })
+        }catch(err) {
+            next(err);
+        };
+        
+        
     }
 
 }
